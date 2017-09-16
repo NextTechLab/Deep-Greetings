4f828d6d201c3b86056f9ff482ab34d7a1a48d59
Aparnaakk <aparnaakk@gmail.com>
2017-09-16 20:10:18 +0530

    Add files via upload

---
+++ halloween_txt.py
@@ -0,0 +1,55 @@
+
+dict1={1:"have they nothing better to do,",
+2:"than haunt my house? ",
+3:"it's getting too frightening",
+4:"for man or mouse!"}
+
+dict2={1:"in masks and gowns we haunt the street,",
+ 2:"and knock on doors for trick or treat.",
+3:"tonight we are the king and queen,",
+4:"for oh tonight it's halloween!"}
+
+dict3={1:"said the third, cover up her ugly toes,",
+2:"her broom,said the fourth, can scratch you--that i know!",
+3:"i'm scared,said the fifth,",
+4:"i think we'd better go."}
+
+dict4={1:"oh no, not again, the time is near,",
+ 2:"it always happens at this time of year.",
+3:"the hissing of unlucky balck cat,",
+ 4:"and ovrhead all the screeching bats."}
+
+dict5={1:"dance and they don't sing,",
+2:"they don't have hair.",
+3:"they don't meow,",
+4:"they don't look much like a cow."}
+
+dict6={1:"shape and size, a pointy nose,",
+ 2:"a jagged mouth and two enormous eyes.",
+3:"we set it in a window and we put a candle in,",
+4:"then lit it up!"}
+
+dict7={1:"up with candy and sweets,",
+ 2:"don't go out on devil's night.",
+ 3:"stay in your bed and keep on the light,",
+ 4:"little demons and ghouls have their fun all right, outside."}
+
+dict8={1:"plumpest sort they sell,",
+ 2:"we neatly scooped the inside out.",
+ 3:"and only left the shell,",
+ 4:"we carved a funny funny-face of silly shape and size."}
+
+dict9={1:"on halloween day,",
+2:"when you know all the ghouls and ghosts are away.",
+3:"they hate the daylight, it's no fun they say,",
+4:"so just please, please wait."}
+
+dict10={1:"see what can't be seen on any other night,",
+ 2:"grinning goblins fighting duels,",
+3:"werewolves rising from their tombs,",
+ 4:"witches on their magic brooms."}
+
+dict11={1:"trick or treat with me?",
+2:"there was an old witch.",
+3:"believe it if you can,",
+4:"she tapped on the windows and she ran, ran, ran."}
