fa31c9a67333e988c96175a606024626ba9586ae
TheBigFundamental <nelaturu.harsha@gmail.com>
2017-09-18 02:17:17 +0530

    Add files via upload

---
+++ Independence.txt.py
@@ -0,0 +1,58 @@
+
+dict1={1:"by our great leaders. let us spread awareness, knowledge, values and gain wisdom.", 
+	   2: "let us make this our independence day resolution."}
+
+dict2 = {1:"love and affection from other citizens of the country as well.",
+	     2:"it is a very important feeling for the country’s freedom." }
+
+dict3 = {1:"the quit india speech, the speech which is",
+		 2:"considered one of the most important one in the history of india.",
+		 2:"no patriotic speech can ever feel complete",
+		 3:"unless it concludes with give me blood, and I shall give you freedom!"}
+
+dict4={1:"in the US, nelson mandela and others of their ilk",
+	   2:"should rule our minds and influence us so that we too",
+       3:"will follow in their footsteps and serve the nation.",
+       4:"it may be said that the patriots’ lives are a reminder"}
+
+dict5={1:"chandra bose, jawaharlal nehru, dr. rajendra prasad,",
+	   2:"and many others. their contribution towards",
+	   3:" attainment of the independence was",
+	   4:"immense and cannot be simply summed",
+	   5:"up in words. however, one thing that", 
+	   6:"requires special mention and which",
+	   7:"we all should imbibe" }
+
+dict6={1:"zeal transgress that thin line which",
+	   2:"can border you towards fanaticism or extremism",
+	   3:"remember that the very foundation of our country",
+	   4:"is founded on the principles of tolerance and compassion.",
+	   5:"patriotism has no room for hatred or ill-feeling."}
+
+dict7={1:"patriots who struggled hard",
+	   2:"for the freedom of india till",
+	   3:"the end of their life.",
+	   4:"we cannot imagine that horrible moments",
+	   5:"struggled by our forefathers.",
+	   6:"now, after many years of independence",
+	   7:"our country is on the right track of development."}
+
+dict8={1:"and its people, then we",
+	   2:"can at least spread the message of love",
+	   3:" and peace everywhere across the globe.",
+	   4:"yet another way to show love for our country",
+	   5:"is by working towards its growth and development",
+	   6:"and taking effective measures"}
+
+dict9={1:"in harmony with over 8 religions",
+	   2:"and celebrate each others festivals with love.",
+	   3:"we welcome people from other country with respect and care.",
+	   4:"we have not forgotten our traditions and values",
+	   5:"and we never let one another to forget them"}
+
+dict10 ={1:"waving, mother of might, mother free",
+		 2: "not where the musk",
+		 3:"of happiness blows, not where darkness",
+		 4:"and fears never tread;",
+		 5:"not in the homes of perpetual smiles,",
+		 6:"nor in the heaven of a land of prosperity would i be born"}
---
+++ Valentines.txt.py
@@ -0,0 +1,124 @@
+dict1={1:"me and loved me for what might or might not be",
+	   2:"– nay, weights and measures do us both a wrong.",
+	   3:"for verily love knows not ‘mine’ or ‘thine;’",
+	   4:"with separate ‘i’ and ‘thou’ free love has done,",
+	   5:"for one"}
+
+dict2={1:"will die too, but not today: you",
+	   2:"incommensurate, therefore the hours shine:",
+	   3:"if i say to you “to you i say",
+	   4:"you have not been set to music,",
+	   5:"or broadcast live on the ghost radio",
+	   6:"may never be an oil"}
+
+dict3={1:"always let you know just what",
+	   2:"You mean to me in my heart my valentine",
+	   3: "is what you'll always be.",
+	   4:"every day i thank the lord above for blessing me",
+	   5:"with you and"}
+
+
+dict4={1:"love with you. from the moment that",
+	   2:"you leave the house to go to work,",
+	   3:"till the moment you walk in through the door again,",
+	   4:"my mind constantly wanders to thoughts of you."}
+
+
+
+dict5={1:"single day the candy will soon disappear",
+	   2:"and the flowers will die away",
+	   3:"but the love that i have for you",
+	   4:"in me that will always stay.",
+	   5:"so here's to you my valentine",
+	   6:"my love and my sweetheart you will"}
+
+dict6={1:"same film over and over,",
+	   2:"and an old one at that,",
+	   3:"shattering its account to the",
+	   4:"last of the digits,",
+	   5:"and nothing and the blank end.",
+	   6:"the lightning has shown me",
+	   7:"the scars of the future."}
+
+dict7={1:"very creative and full of love.",
+	   2:"the love of your life will feel",
+	   3:"happy that you spend some of these.",
+	   4:"i love you with all my heart right",
+	   5:"from the very start our love grows",
+	   6:"stronger everyday with everything you do"}
+-
+ 
+
+dict8={1:"i adore you in the summer,",
+	   2:"when the air is filled with heat;",
+	   3:"without you in my life each day,",
+	   4:"i wouldn't be complete. i treasure you in fall",
+	   5:"when leaves are turning gold;"}
+
+dict9={1:"so hard to do but the three words",
+	   2:"that i'll always say will be",
+	   3:"i love you. you will always be",
+	   4:"my valentine my love and my sweetheart",
+	   5:"and for you i'll give you everything",
+	   6:"especially my heart and i"}
+
+
+dict10={1:"have ravish’d me away by a power",
+		2:"i cannot resist: and yet i",
+		3:"could resist till i saw you;",
+		4:"and even since i have seen you i have",
+		5:"endeavoured often “to reason against the reasons of my love"}
+
+
+dict11={1:"so much in love with you that",
+		2:"i think of you only every single minute",
+		3:"of the day and wish that i was with you!",
+		4:"i can't stand to be without you for even a second.",
+		5:"you have truly turned me stove"} 
+
+
+dict12={1:"a valentine is grounded, centered, peaceful, a role model.",
+		2:"my valentine is you. on valentine's day,",
+		3:"i think of people who make life better,",
+		4:"happier, just by being around,",
+		5:"people whose good nature,",
+		6:"ready smile and eagerness to help enrich"}
+
+
+
+dict13={1:"clip, and y your tight skirt the music picks up again",
+		2:"i am the man i hope to be the bright air",
+		3:"hangs freely near your newly cut hair",
+		4:"it is so easy now to see gravity at work"}
+
+
+
+dict14={1:"you as much and as well as i can.",
+		2:"i hope you know that i have the best",
+		3:"intentions always, so forgive me if",
+		4:"i ever hurt you or caused you pain.",
+		5:"i love you for now until forever and will"} 
+
+dict15={1:"my mind, i always have such pleasant thoughts of you.",
+		2:"you bring me so much happiness and joy;",
+		3:"those who lift me up are very few.",
+		4:"on valentine's day i want to tell you this:",
+		5:"knowing you is extraordinary"}
+
+
+
+
+dict16={1:"who we think of with a smile.",
+		2:"i am fortunate to know you,",
+		3:"that's why i want to say,",
+		4:"to a rare and special person:",
+		5:"happy valentine's day!"}
+
+
+dict17={1:"for my soul. you nourish me, sweetheart;",
+		2:"you make me whole. keep me satisfied,",
+		3:"stay close or i'll pine.",
+		4:"i sure get hungry thinking of you,",
+		5:"valentine! be mine, and let's nourish each other!",
+		6:"i hope your valentine's day is amazing"}
+
